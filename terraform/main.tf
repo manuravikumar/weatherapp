81d0716f76bb613d15824de0eb24bf8e9189661a
manuravikumar <65894900+manuravikumar@users.noreply.github.com>
2025-04-28 12:07:11 +1000

    updating main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,12 @@
-provider "azurerm" {
-  features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.98"  # or latest stable version you want
+    }
+  }
 }
+
 terraform {
   backend "azurerm" {}
 }
